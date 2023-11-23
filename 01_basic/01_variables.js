797c7c08c1cb2cf253005e89be3f21a0c9de0967
sumitrow <151813915+sumitrow@users.noreply.github.com>
2023-11-23 07:40:59 +0000

    dataTypes

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 14256;
+let accountImail = "sk1234@gmail.com";
+var accountPassword = "12356";
+accountCity = "jaipur"
+let accountState;
+
+// accountId = 2
+
+accountImail = "hf@gmail.com"
+
+accountPassword = "32145"
+
+accountCity = "delhi"
+
+
+console.log(accountId);
+
+console.table([accountId, accountImail, accountPassword, accountCity, accountState])
---
+++ 01_basic/02_dataType.js
@@ -0,0 +1,23 @@
+
+
+console.log("hello");
+
+let name = "sumit"
+let age = 18
+let isLogged = false
+let state = null
+
+// number => to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null =>
+// undefined =>
+// symbols => unique
+
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // Object
+
+
+ 
