d423b338fc014674e9c141ffaa9c3360806ef83a
sumitrow <151813915+sumitrow@users.noreply.github.com>
2023-11-24 06:48:07 +0000

    change in location file

---
+++ 01_basic/datatype_summary.js
@@ -0,0 +1,45 @@
+// primitive datatype
+
+
+// 7 type : string, number, boolearn, Null,undefnede, symbol, bigInt
+
+
+const score = 100
+const scorevalue = 100.3
+
+const isLoggedIn = false
+const outsideTem = null
+let userEmai;
+
+const id = Symbol(123)
+const anotherId =  Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 163846168416816n
+
+
+const heros = ["Saktiman", "nagrag", "surya", "kartik Aryan"];
+let myObj = {
+    name: "sumit",
+    age: 22,
+}
+function myFunction () {
+    console.log("hello Sumit b");
+    
+}
+
+// console.log(typeof outsideTem);
+// console.log(typeof myFunction);
+console.log(typeof anotherId);
+
+
+
+
+
+
+
+
+// referance (non primitive)
+
+// array, objects, functions
