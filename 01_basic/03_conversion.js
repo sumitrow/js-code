ee51563d77b28d297b75d9032eb244f3fec1aa7d
sumitrow <151813915+sumitrow@users.noreply.github.com>
2023-11-24 05:22:30 +0000

    conversion of datatype

---
+++ 01_basic/03_conversion.js
@@ -0,0 +1,27 @@
+// let score = true
+ 
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber)
+
+
+// "30" => 33 
+// "33aaa" => NaN
+// true => 1; false => 0
+
+// let isLoggedIn = 1
+// let isLoggedIn = "1"
+// let isLoggedIn = "sumit"
+let isLoggedIn = 0
+let boolean = Boolean(isLoggedIn)
+console.log(boolean);
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
