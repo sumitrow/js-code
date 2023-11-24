7d8ab52476185d84a975e67413ff10c5c200af78
sumitrow <151813915+sumitrow@users.noreply.github.com>
2023-11-24 06:15:14 +0000

    comparisions of chek

---
+++ 01_basic/04_comparisions.js
@@ -0,0 +1,13 @@
+// console.log(2 > 1);
+
+console.log(typeof "2" + 1);
+console.log("2" - 1);
+console.log("2" * 1);
+console.log("2" / 1);
+console.log("2" % 1);
+console.log("2" == 1);
+
+
+console.log(undefined == 0);
+console.log(undefined < 0);
+console.log(undefined > 0);
