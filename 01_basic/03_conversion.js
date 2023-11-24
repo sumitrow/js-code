d1aefd4f7b89e447b84fdb02265191806d72d318
sumitrow <151813915+sumitrow@users.noreply.github.com>
2023-11-24 05:49:07 +0000

    conversion of oprations

--- 01_basic/03_conversion.js
+++ 01_basic/03_conversion.js
@@ -16,12 +16,39 @@
 // let isLoggedIn = 1
 // let isLoggedIn = "1"
 // let isLoggedIn = "sumit"
-let isLoggedIn = 0
-let boolean = Boolean(isLoggedIn)
-console.log(boolean);
+// let isLoggedIn = 0
+// let boolean = Boolean(isLoggedIn)
+// console.log(boolean);
 
 
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ***************** oprations *******************
+
+// let value = 3
+// let nagvalue = -value
+// console.log(nagvalue);
+
+
+// let str1 = "hello"
+// let str2 = " Sumit"
+// let str3 = str1+str2
+// console.log("1" + 2);
+// console.log("1" + 2 + 20);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+console.log(+"");
+
+let num1, num2, num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+++gameCounter;
+console.log(gameCounter);
+
+// link to study = https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
