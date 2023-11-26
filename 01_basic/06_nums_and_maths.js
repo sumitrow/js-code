1d7b7ec51b8ebd72e6369d56db8f78dbf973df88
sumitrow <151813915+sumitrow@users.noreply.github.com>
2023-11-26 07:17:48 +0000

    math and nums

---
+++ 01_basic/06_nums_and_maths.js
@@ -0,0 +1,43 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+
+
+
+// console.log(balance.toFixed(2));
+
+const otherNumber = 23.544
+// console.log(otherNumber.toPrecision(1));
+
+const hundred = 100000
+// console.log(hundred.toLocaleString('en-IN'));
+
+
+//  **************************** maths ******************************
+
+// const mathss = Math
+
+
+/*
+console.log(Math)
+console.log(Math.abs(-4));
+console.log(Math.round(4.4));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
+
+*/
+
+console.log(Math.random());
+console.log(Math.floor(Math.random()*10) + 1);
+const min = 10
+const max = 20
+
+ console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+
+
+
+
