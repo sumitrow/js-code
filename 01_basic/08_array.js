dc213956971fae761a09a8502b0675cdc39c7750
sumitrow <151813915+sumitrow@users.noreply.github.com>
2023-11-27 06:18:27 +0000

    save the array in js

---
+++ 01_basic/08_array.js
@@ -0,0 +1,32 @@
+const myArray = [1,2,3,4,5,6, "sumit"]
+// const newArray = ["rahul", "satyam"]
+
+// console.log(myArray);
+
+// array medhods
+
+// myArray.push(6)
+// myArray.push(7)
+// myArray.pop()
+
+// myArray.unshift(0)
+// myArray.shift()
+
+
+// const newArray1 = myArray.join()
+
+
+// console.log(myArray);
+// console.log(typeof newArray1);
+
+
+// slice splice
+
+console.log("A", myArray);
+const myn1 = myArray.slice(1,3)
+console.log(myn1);
+console.log("B", myArray);
+
+const myn2 = myArray.splice(2,3)
+console.log(myn2)
+console.log(myArray);
