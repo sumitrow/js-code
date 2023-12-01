99887a9a081f68782912b3be20eabd3dcb6d7a9f
sumitrow <151813915+sumitrow@users.noreply.github.com>
2023-12-01 07:16:22 +0000

    for in loop

---
+++ 03_iterations/for.js
@@ -0,0 +1,29 @@
+
+// const myobj = {
+//     js: 'javascript',
+//     cpp: "c++",
+//     rb: 'ruby',
+//     swift: "swift by apple"
+
+// }
+// for (const key in myobj) {
+//     console.log(`${key} sortcut is for : ${myobj[key]}`);
+ 
+// }
+
+// const programing = ["js","py","C","java","cpp"]
+// for (const key in programing) {
+//     console.log(programing[key]);
+  
+// }
+
+// const newmap = new Map()
+// newmap.set('in', "india")
+// newmap.set('usa', "america")
+// newmap.set('dl', "delhi")
+// newmap.set('in', "india")
+
+// for (const key in newmap) {
+//     console.log(key);
+  
+// }
